009d277f0c5e55529b3736b66845dc6f8d1653d6
ubaid-ullah <ubaidullah31357@gmail.com>
2026-05-20 10:07:52 +0500

    feat: initialize React project with Vite, add basic structure and styles
    - Created package.json with dependencies and scripts for development
    - Added global CSS styles in App.css including font declarations
    - Implemented main App component and a Header component for navigation
    - Included font files for Urbanist and Quicksand with respective licenses
    - Set up main entry point in main.jsx and Vite configuration in vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Headers } from "./components/UI/Headers";
+
+const App = () => {
+  return (
+    <>
+      <Headers />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/UI/Headers.jsx
@@ -0,0 +1,25 @@
+export const Headers = () => {
+  return (
+    <div className="header-section">
+      <div className="logo">
+        <p>
+          Ubaid <span className="green-style">/.</span>
+        </p>
+      </div>
+      <div className="navbar-and-btn">
+        <nav className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Experience</li>
+            <li>Contact Me</li>
+          </ul>
+        </nav>
+        <div className="resume-btn">
+          <button>Visit Resume</button>
+        </div>
+      </div>
+    </div>
+  );
+};
